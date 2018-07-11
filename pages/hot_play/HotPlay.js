5a4dca4a0f46f3643ab70d7ab4c7d8c9c03c2499
xiaofeng <724332274@qq.com>
2018-07-11 12:09:42 +0800

    添加了热映电影列表的上拉加载功能
    添加了热映电影列表的上拉加载功能，另外请求API时带上了当前城市

--- pages/hot_play/HotPlay.js
+++ pages/hot_play/HotPlay.js
@@ -1,47 +1,61 @@
 // pages/hot_play/HotPlay.js
+
+var 
+    app = getApp(),
+    start = 0, // 起始索引位置
+    count = 10, // 每次查询数量
+    total = 0;
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    hotPlay: []
+    hotPlay: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let _this = this;
-    wx.showLoading({ title: "加载中...", mask: true });
-    wx.request({
-      url: 'https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=武汉&start=0&count=10',
-      method: 'GET',
-      header: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      success: function (res) {
-          wx.hideLoading();
-          _this.setData({
-              hotPlay: res.data
-          });
-        console.log(res.data)
-      }
-    })
+      this.getHotPlayMovie();
+  },
+
+  // 请求热映数据
+  getHotPlayMovie: function(){
+      if(start > total){return}
+      let _this = this;
+      wx.showNavigationBarLoading();
+      wx.request({
+          url: `https://api.douban.com/v2/movie/in_theaters?apikey=${app.globalData.apiKey}=${wx.getStorageSync('city')}&start=${start}&count=${count}`,
+          method: 'GET',
+          header: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          success: function (res) {
+              wx.hideNavigationBarLoading();
+              _this.setData({
+                  hotPlay: [..._this.data.hotPlay, ...res.data.subjects]
+              });
+              start += count;
+              total = res.data.total;
+          }
+      })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+        
   },
 
   /**
@@ -69,7 +83,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+      this.getHotPlayMovie();
   },
 
   /**
